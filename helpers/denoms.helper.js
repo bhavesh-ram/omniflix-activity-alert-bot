9fead847708eaadaabc6858f075108a6da90d033
BlockEater <Hariom@cosmiclabs.co>
2022-12-08 16:06:40 +0530

    notification sending added for all type

---
+++ helpers/denoms.helper.js
@@ -0,0 +1,134 @@
+const https = require('https')
+const { ActivityData } = require("../models/activity.model");
+const { userData } = require('../models/user.model');
+const dotenv = require('dotenv').config()
+
+
+let transferDenomHelper = async (activity) => {
+    try {
+
+
+        // console.log("transferDenomHelper");
+        // let id = activity;
+        let id = activity._id;
+        let activityData = await ActivityData.findOne({ _id: id });
+        if (!activityData) {
+            console.log("Acitivity not found");
+        }
+
+
+
+        let creatorData = await userData.find({
+            isSubscribe: true,
+            omniflixAddress: activityData.creator
+
+        })
+
+        // owner is receipent in this context
+        let ownerData = await userData.find({
+            isSubscribe: true,
+            omniflixAddress: activityData.recipient
+        })
+
+        let denomId = activityData.id;
+
+        if (creatorData.length) {
+            creatorData.forEach((data) => {
+                let msg = `You have transferred this collection to ${activityData.recipient} %0A(https://omniflix.market/collection/${denomId})`;
+                let userId = data.userId;
+                let target = `https://api.telegram.org/bot${process.env.token}/sendMessage?chat_id=${userId}&text=${msg}&parse_mode=markdown`
+                https.get(target, (res) => {
+                    console.log("Notification sent");
+                })
+
+            })
+        }
+
+
+        if (ownerData.length) {
+            ownerData.forEach((data) => {
+                let msg = `You have received the below collection from %0A${activityData.creator} %0A(https://omniflix.market/collection/${denomId})`;
+                let userId = data.userId;
+                let target = `https://api.telegram.org/bot${process.env.token}/sendMessage?chat_id=${userId}&text=${msg}&parse_mode=markdown`
+                https.get(target, (res) => {
+                    console.log("Notification sent");
+                })
+
+            })
+        }
+
+
+
+        await ActivityData.findOneAndUpdate({
+            _id: id
+        }, {
+            $set: {
+                isNotified: true
+            }
+        })
+
+        console.log("all working")
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+let updateDenomHelper = async (activity) => {
+    try {
+        console.log("updateDenomHelper working ")
+        let id = activity._id;
+        // let id = activity;
+        let activityData = await ActivityData.findOne({ _id: id });
+        if (!activityData) {
+            console.log("Acitivity not found");
+        }
+
+        let denomId = activityData.id;
+
+        let ownerData = await userData.find({
+            isSubscribe: true,
+            omniflixAddress: activityData.creator
+        })
+
+        let symbol = activityData.symbol;
+        let name = activityData.name;
+        // let description = activityData.description;
+
+        // console.log(ownerData)
+
+
+
+        if (ownerData.length) {
+
+            ownerData.forEach((data) => {
+
+
+
+                let ownerMsg = `Your collection has been updated  %0ASymbol: ${symbol} %0AName: ${name} %0AClick this link to check: (https://omniflix.market/collection/${denomId})`;
+
+                let ownerTarget = `https://api.telegram.org/bot${process.env.token}/sendMessage?chat_id=${data.userId}&text=${ownerMsg}&parse_mode=markdown`
+
+                https.get(ownerTarget, (res) => {
+                    console.log("Notification sent")
+                })
+            })
+
+        }
+
+        await ActivityData.findOneAndUpdate({
+            _id: id
+        }, {
+            $set: {
+                isNotified: true
+            }
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { transferDenomHelper, updateDenomHelper }
+
+
--- src/mainJobScheduler.js
+++ src/mainJobScheduler.js
@@ -2,47 +2,63 @@ let request = require("request")
 var cron = require('node-cron');
 const dotenv = require('dotenv').config()
 const { ActivityData } = require("../models/activity.model");
-const { auctionHelper } = require("../helpers/auctions.helpers");
-const { listingHelper } = require("../helpers/listings.helpers");
+const { createAuctionHelper, cancelAuctionHelper, removeAuctionHelper, processBidAuctionHelper, placeBidAuctionHelper } = require("../helpers/auctions.helpers");
+const { listingHelper, deListingHelper } = require("../helpers/listings.helpers");
+const { transferNftHelper } = require("../helpers/transferNft.helper");
+const { buyNftHelper, burnNftHelper, mintONFTHelper } = require("../helpers/buyNfts.helpers");
+const { updateDenomHelper, transferDenomHelper } = require("../helpers/denoms.helper");
 
 
-let MainSheduler = async () =>{
+let MainScheduler = async () =>{
     await ActivityData.find({
-        "isNotified":false
+        "isNotified":false    
     }, {}, {
         sort: {
             created_at: 'desc'
         },
-        limit: 4
+        limit: 5
     }, async (error, activities) => {
+        // console.log(activities)
         if (error) {
             console.log(error)
         }else if(activities && activities.length){
-            activities.forEach(async (activity) =>{
+            activities.forEach((activity) =>{
                 if(activity.type == "MsgCreateAuction"){
-                    // mintHelper(activity)
-                    let auctionData = await auctionHelper(activity)
-                    console.log("aa",auctionData)
-                }else if(activity.type == "MsgPlaceBid"){
-                    // mintHelper(activity)
+                    createAuctionHelper(activity)
                 }else if(activity.type == "MsgListNFT"){
                     listingHelper(activity)
-
-                // }else if(activity.type == "MsgListNFT"){
-                //     // mintHelper(activity)
-                // }else if(activity.type == "MsgMintONFT"){
-                //     // mintHelper(activity)
-                // }else if(activity.type == "MsgMintONFT"){
-                    // mintHelper(activity)
-                }
+                }else if(activity.type == "MsgCancelAuction"){
+                    cancelAuctionHelper(activity)
+                }else if(activity.type == "RemoveAuction"){
+                    removeAuctionHelper(activity)
+                }else if(activity.type == "ProcessBid"){
+                    processBidAuctionHelper(activity)
+                }else if(activity.type == "MsgPlaceBid"){
+                    placeBidAuctionHelper(activity)
+                }else if(activity.type == "MsgTransferONFT"){
+                    transferNftHelper(activity)
+                }else if(activity.type == "MsgBuyNFT"){
+                    buyNftHelper(activity)
+                 }else if(activity.type == "MsgDeListNFT"){
+                    deListingHelper(activity)
+                 }else if(activity.type == "MsgBurnONFT"){
+                    burnNftHelper(activity)
+                 }else if(activity.type == "MsgUpdateDenom"){
+                    updateDenomHelper(activity)
+                 }else if(activity.type == "MsgTransferDenom"){
+                    transferDenomHelper(activity)
+                 }else if(activity.type == "MsgMintONFT"){
+                    mintONFTHelper(activity)
+                 }
             })
+
         }
     })
 }
 
 
-let mainShedulerData = cron.schedule('*/10 * * * * *', MainSheduler)
+let mainSchedulerData = cron.schedule('*/10 * * * * *', MainScheduler)
 
 module.exports ={
-    mainShedulerData
+    mainSchedulerData
 }
