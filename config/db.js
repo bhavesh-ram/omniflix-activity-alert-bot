33b98f61aab6205ab7938b8ad419328f59080960
OmniflixBlockEater <hariom@cosmiclabs.co>
2022-12-23 20:45:02 +0530

    updated the bulk auction notification

--- config/db.js
+++ config/db.js
@@ -7,12 +7,12 @@ const connectDB = async () =>{
             // console.log("aaa")
             mongoose.connect(`${process.env.DATABASE_URI}/${process.env.DATABASE_NAME}`,{useNewUrlParser: true, useUnifiedTopology: true})
             // process.env.MONGO_URI
-            console.log("database connected");
+            console.log(`database connected to ${process.env.DATABASE_URI}/${process.env.DATABASE_NAME}`);
         }else{
             // console.log("aa")
             mongoose.connect('mongodb://localhost:27017/marketplace_bot',{useNewUrlParser: true, useUnifiedTopology: true})
             // process.env.MONGO_URI
-            console.log("database connected");
+            console.log("database connected to mongodb://localhost:27017/marketplace_bot");
         }
         
 
